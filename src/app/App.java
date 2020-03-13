2091747eabd7df49c0f44233d2f09cba7a53f8b3
Quang Thanh Ta <derthanh001@gmail.com>
2020-03-13 16:28:34 +0100

    CSV-Daten

--- src/app/App.java
+++ src/app/App.java
@@ -1,7 +1,12 @@
 package app;
 
+import components.GameMasterCSV;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello Java");
+        GameMasterCSV gameMasterCSV = new GameMasterCSV(1000000, 10, 0);
+
+        gameMasterCSV.playTurns(1000);
     }
 }
